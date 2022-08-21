35b253fd70b11783cce9490a94681eb28b669979
Lucca Barroso <barrosolucca@gmail.com>
2022-08-21 14:32:14 -0300

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/pages/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#111111',
+        'light-gray': '#090B0C',
+        'green-gray': '#070D0A'
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+//npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
